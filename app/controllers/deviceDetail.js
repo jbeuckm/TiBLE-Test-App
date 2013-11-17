88d30799ee4fa6ad5f5c1b941ae4dab5d2b5160e
joe beuckman <joebeuckman@gmail.com>
2013-11-16 20:27:28 -0600

    a sectioned table of service and characteristic uuids

--- app/controllers/deviceDetail.js
+++ app/controllers/deviceDetail.js
@@ -1,35 +1,71 @@
 var args = arguments[0] || {};
 
-
 $.deviceModel.set(args.model);
+
+$.activityIndicator.show();
 
 Alloy.Globals.ble.connectPeripheral($.deviceModel.get("uuid"));
 
-function backButton() {
-	$.win.close();
-}
-
-
-//$.servicesTable.add();
 
 var ble = Alloy.Globals.ble;
 
-ble.addEventListener("connect", function(e){
-	alert(e);
-});
-ble.addEventListener("connectFail", function(e){
-	alert(e);
-});
-ble.addEventListener("disconnect", function(e){
-	alert(e);
-});
+ble.addEventListener("connect", connect);
+ble.addEventListener("connectFail", connectFail);
+ble.addEventListener("disconnect", disconnect);
+ble.addEventListener("services", services);
+ble.addEventListener("characteristics", characteristics);
+ble.addEventListener("characteristicValue", characteristicValue);
 
-ble.addEventListener("services", function(e){
-	alert(e);
-});
-ble.addEventListener("characteristics", function(e){
-	alert(e);
-});
-ble.addEventListener("characteristicValue", function(e){
-	alert(e);
-});
+
+function connect(e) {
+    $.activityIndicator.hide();
+}
+function connectFail(e) {
+    alert(e);
+}
+function disconnect(e) {
+    
+}
+
+var sections = [];
+function services(e) {
+    for (var i in e.services) {
+        var s = e.services[i];
+        var section = Ti.UI.createTableViewSection({headerTitle:s.uuid});
+        $.servicesTable.appendSection(section);
+        
+        sections[s.uuid] = section;
+    }
+}
+
+var characteristicRows = [];
+function characteristics(e) {
+    var serviceUUID = e.service.uuid;
+    var section = sections[serviceUUID];
+    
+    for (var i in e.characteristics) {
+        var c = e.characteristics[i];
+        
+        var row = Ti.UI.createTableViewRow({ title: c.uuid });
+        characteristicRows[c.uuid] = row;
+        section.add(row);
+    }
+}
+function characteristicValue(e) {
+    
+}
+
+
+function backButton() {
+    ble.removeEventListener("connect", connect);
+    ble.removeEventListener("connectFail", connectFail);
+    ble.removeEventListener("disconnect", disconnect);
+    ble.removeEventListener("services", services);
+    ble.removeEventListener("characteristics", characteristics);
+    ble.removeEventListener("characteristicValue", characteristicValue);
+    
+    sections = null;
+    characteristics = null;
+
+    $.win.close();
+}
